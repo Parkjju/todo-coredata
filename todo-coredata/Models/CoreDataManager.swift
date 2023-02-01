82120e1386b6f3414303dfc8132fdb3258fdb283
Parkjju <rudwns3927@gmail.com>
2023-02-02 08:52:59 +0900

    feat: 코어데이터 매니저 모델 추가

---
+++ todo-coredata/Models/CoreDataManager.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataManager.swift
+//  todo-coredata
+//
+//  Created by Jun on 2023/02/02.
+//
+
+import Foundation
+
+class CoreDataManager{
+    static let shared = CoreDataManager()
+    
+    private init(){}
+}
