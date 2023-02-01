01982451a5aba574addbfb02b9e399d253ee7a65
Parkjju <rudwns3927@gmail.com>
2023-02-01 23:51:34 +0900

    Initial Commit

---
+++ todo-coredata/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo-coredata
+//
+//  Created by Jun on 2023/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
